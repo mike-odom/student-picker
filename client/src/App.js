a191d302f07cd95d1f89eb4c61f290c7ca276659
Mike Odom <5105729+mike-odom@users.noreply.github.com>
2019-04-08 21:51:14 -0700

    Updating header text

--- client/src/App.js
+++ client/src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>YOU'RE GONNA TURN THIS BAREBONES BOILERPLATE INTO SOMETHING AMAZING!</h2>
+          <h2>Student Picker</h2>
         </div>
         <p className="App-intro">
           <MotivationalQuote />
